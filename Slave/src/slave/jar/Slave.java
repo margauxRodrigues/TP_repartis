d5bb57db4449ab3b4280124b635afd8921e85940
margauxRodrigues <margaux.rodrigues@telecom-paristech.fr>
2018-10-09 14:36:45 +0200

    slave.jar modified to wait 10 seconds

---
+++ Slave/src/slave/jar/Slave.java
@@ -0,0 +1,11 @@
+package slave.jar;
+
+public class Slave {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		Thread.sleep(10000);	
+		int a = 3 + 5;
+		System.out.println(a);
+	}
+}
